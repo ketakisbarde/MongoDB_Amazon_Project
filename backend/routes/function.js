31922e481e34031b4f10c59dc63c64892b949b9e
sna2485 <saloni.aggarwal2509@gmail.com>
2022-04-08 01:00:25 -0400

    changes to function.js

--- backend/routes/function.js
+++ backend/routes/function.js
@@ -9,13 +9,59 @@ let amazonData = require('../models/dataModel');
 
 // This is the first end point that handle http get request
 // router.get()
-router.route('/').get((req, res) => {
-    // res.send("CONNECTED TO GET ROUTE");
-    //Mongoose method to get list of all data from database 
-    amazonData.find().limit(10)
-    .then(data => res.json(data))
-    .catch(err => res.status(400).json('Error: ' +err));
+// router.route('/').get((req, res) => {
+//     // res.send("CONNECTED TO GET ROUTE");
+//     //Mongoose method to get list of all data from database 
+//     amazonData.find().limit(10)
+//     .then(data => res.json(data))
+//     .catch(err => res.status(400).json('Error: ' +err));
+// })
+
+// router.route('/:id').get((req, res) => {
+//     amazonData.findById(req.params.id)
+//     .then(data => {
+//         // console.log(data);
+//         res.json(data)
+//     })
+//     .catch(err => res.json(400).json('Error: ' +err));
+
+// })
+
+router.route('/search/:description').get((req, res) => {
+    amazonData.find({$or: [{name:{$regex: new RegExp(req.params.description, 'i')}}, {categories:{$regex: new RegExp(req.params.description, 'i')}}]})
+    .then(data => {
+        res.json(data)
+    })
+    .catch(err => res.json(400).json('Error: ' +err));
 })
+
+router.route('geoSearch/:longitude/:latitude').get((req, res) => {
+    var long = req.params.longitude;
+    var lat = req.params.latitude;
+    amazonData.find({
+        geoLoc: {
+            $near: {
+                $geometry: {
+                    type: "Point",
+                    coordinates: [long, lat]
+                },
+                $maxDistance: 500
+            }
+        }
+    })
+    .then(data => {
+        res.json(data)
+    })
+    .catch(err => res.json(400).json('Error: ' +err));
+})
+
+// router.route('/searchPage/:categories').get((req, res) => {
+//     amazonData.find({"categories":{$regex: new RegExp(req.params.categories, 'i')}})
+//     .then(data => {
+//         res.json(data)
+//     })
+//     .catch(err => res.json(400).json('Error: ' +err));
+// })
 
 //Exporting the router
 module.exports = router;
